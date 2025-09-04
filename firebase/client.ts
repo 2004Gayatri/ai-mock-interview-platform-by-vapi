d5c50f7d52b7a1a35cfad776fa6e8c44deca9c65
gayatri2004 <gayatrisanap2572004@gmail.com>
2025-09-05 00:12:59 +0530

    updating client

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,25 +1,4 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp , getApp,getApps} from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCic6FEFE-L7Kkis0qyZTUvhKklgkfog6Q",
-//   authDomain: "prepwise-d1ccd.firebaseapp.com",
-//   projectId: "prepwise-d1ccd",
-//   storageBucket: "prepwise-d1ccd.firebasestorage.app",
-//   messagingSenderId: "831772117488",
-//   appId: "1:831772117488:web:59e4145b26d335a01fb535",
-//   measurementId: "G-PN1SZM3TRQ"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";

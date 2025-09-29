d96dfd24761f7c8092775abb05d858340d726792
gayatri2004 <gayatrisanap2572004@gmail.com>
2025-09-29 17:23:33 +0530

    Adding some modificaions

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,21 @@
-import React from "react";
 import Agent from "@/components/Agent";
-const page = () =>{
-    return(
-        <>
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-         <h3>Interview Generation</h3>
-         <Agent userName="You" userId="user1" type="generate" />
+const Page = async () => {
+  const user = await getCurrentUser();
 
-        </>
-    )
-}
+  return (
+    <>
+      <h3>Interview generation</h3>
 
-export default page
+      <Agent
+        userName={user?.name || "Guest"}
+        userId={user?.id || ""}
+        // profileImage={user?.profileURL || "/user-avatar.png"}
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
